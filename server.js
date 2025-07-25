529acf1dad37214707487b08e0327041a457e614
Jawahar <jawahar@cannymindstech.com>
2025-07-25 17:57:16 +0530

    Added: DOM functionalities also node start in server.js

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 let num = 5.9;
+
+const os = require('os');
 console.log(Math.floor(num))
 console.log(`Items (${1+1}): $${(num*10+3176)/100}`)
+
+/*
+console.log(os.type());
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.version());
+console.log(os.homedir()) 
+console.log(__dirname);
+console.log(__filename);
+console.log("------");
+*/
+const math = require('./math.js');
+
+console.log(math.add(5,3));
